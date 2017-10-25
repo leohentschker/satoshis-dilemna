659ec6c525d17cff780a16b118a8abc384ecea26
Leo Hentschker <leohentschker@college.harvard.edu>
2017-10-24 23:49:44 -0400

    Integrating with infura

--- eth/truffle.js
+++ eth/truffle.js
@@ -3,8 +3,15 @@ require('regenerator-runtime/runtime')
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 
+const testNetwork = require('./engines/testEngine.js')
+
 module.exports = {
   networks: {
+    "ropsten": {
+      network_id: 3,
+      provider: testNetwork.testEngine,
+      from: testNetwork.testAddress,
+    },
     "test": {
       network_id: 1,
       host: 'localhost',
